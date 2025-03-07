28cadb9e94b745eda3e7aa09b127ccc76328b5a4
TYHTTT3850 <Tyh11811@outlook.com>
2025-03-07 15:02:33 +0800

    Delete MySQL-Learning/SQL基础 directory

--- MySQL-Learning/SQL基础/01-DDL语句/案例练习/设计一张员工表.sql
+++
@@ -1,9 +0,0 @@
-create table emp(
-num int comment '编号',
-id_work varchar(10) comment '员工工号',
-name varchar(10) comment '员工姓名',
-gender char(1) comment '性别',
-age tinyint unsigned comment '年龄',
-id_number char(18) comment '身份证号',
-join_time date comment '入职时间'
-) comment '员工表';
