3e09542ae6b8d0bdd54ef24f90da17c53c8c7bc7
TYHTTT3850 <Tyh11811@outlook.com>
2025-03-13 13:53:47 +0800

    Create 修改表.sql

---
+++ MySQL-Learning/Ch01-SQL基础/01-DDL语句/学习脚本/修改表.sql
@@ -0,0 +1,12 @@
+create table employee(id int , workno int, name varchar(10), gender char(1), age tinyint unsigned,
+                      idcard char(18), workdate date);
+
+alter table employee add nickname varchar(20) comment '昵称'; # 增加字段
+
+alter table employee modify gender char(2); # 修改字段的数据类型
+
+alter table employee change nickname username varchar(30); # 修改字段名和字段的数据类型
+
+alter table employee drop age; # 删除字段
+
+alter table employee rename employee1; # 修改表名
