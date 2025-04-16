012bbc0d42b215fb78afad0d5f80cc8edc047971
TYHTTT3850 <Tyh11811@outlook.com>
2025-04-17 01:39:32 +0800

    Update 基本的注释和输入输出.cpp

--- Cpp-Learning/C++学习代码/基本的注释和输入输出.cpp
+++ Cpp-Learning/C++学习代码/基本的注释和输入输出.cpp
@@ -7,7 +7,7 @@ using namespace std; // 使用std命名空间
 int main() {// main函数为程序的入口
     int x{0};
     // 标准输入
-    cin >> x; 
+    cin >> x; //用户输入的值存进x
     // 标准输出
     cout << "Hello World!" << endl; // endl换行并刷新缓冲区
 }
