e9e0bfb28c041de2cfc542ba9f7c53c6a79921c5
TYHTTT3850 <Tyh11811@outlook.com>
2025-04-17 01:39:05 +0800

    Update and rename 基本的注释和输出.cpp to 基本的注释和输入输出.cpp

--- Cpp-Learning/C++学习代码/基本的注释和输入输出.cpp
+++ Cpp-Learning/C++学习代码/基本的注释和输入输出.cpp
@@ -5,5 +5,9 @@ using namespace std; // 使用std命名空间
 // 注释
 
 int main() {// main函数为程序的入口
+    int x{0};
+    // 标准输入
+    cin >> x; 
+    // 标准输出
     cout << "Hello World!" << endl; // endl换行并刷新缓冲区
 }
