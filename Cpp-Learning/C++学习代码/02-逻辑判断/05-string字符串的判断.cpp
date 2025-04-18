163a200a638d61772c0688522e89de93ede40471
TYHTTT3850 <Tyh11811@outlook.com>
2025-04-19 01:24:01 +0800

    Update and rename 05-string字符串的长度、截断和比较.cpp to 05-string字符串的判断.cpp

--- Cpp-Learning/C++学习代码/02-逻辑判断/05-string字符串的判断.cpp
+++ Cpp-Learning/C++学习代码/02-逻辑判断/05-string字符串的判断.cpp
@@ -3,15 +3,6 @@
 using namespace std;
 
 int main() {
-    // 字符串长度
-    string str4{"123456789"};
-    cout << "str4存储了几个字符，不包括\\0：" << str4.size() << endl;
-    cout << "str4实际能用的空间" << str4.capacity() << endl;
-
-    // 截断字符串
-    cout << str4.substr(3) << endl; //从索引为 3 处开始取到最后
-    cout << str4.substr(1, 3) << endl; // 从索引为 1 处开始,取 3 个
-
     // 空串判断
     string strif;
     if (strif.empty()) {// 最高效,一般都用这个
