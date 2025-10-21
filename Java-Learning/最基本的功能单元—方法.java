6bca13a5cbbfe46811f09d8ff35a050722503564
TYHTTT3850 <Tyh11811@outlook.com>
2025-10-22 03:13:14 +0800

    创建 最基本的功能单元—方法.java

---
+++ Java-Learning/最基本的功能单元—方法.java
@@ -0,0 +1,10 @@
+public class Function {
+	//方法是java最基本的功能单元
+	public static int sum(int a,int b){
+		return a+b;
+	}
+    public static void main(String []args) {
+        int s = sum(3,5);
+		System.out.println(s);
+    }
+}
