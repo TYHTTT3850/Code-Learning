d8ea6276b60fd3aacc02c28cdb87066a8f13a1d7
TaoYuhang <Tyh11811@outlook.com>
2025-11-06 00:54:01 +0800

    Update 01-面向对象基础语法.java

--- Java-Learning/Ch02-面向对象/01-面向对象基础语法.java
+++ Java-Learning/Ch02-面向对象/01-面向对象基础语法.java
@@ -4,10 +4,10 @@ public static class Person{//创建一个类
 
     public Person(String name, int age){//类的构造方法
         this.name = name;//this代表当前对象
-        this.age = age;
+        this.age = age;//成员变量
     }
 
-    public String getName(){
+    public String getName(){//成员方法
         return name;
     }
 
@@ -18,5 +18,6 @@ public static class Person{//创建一个类
 public static void main(String[] args){
     Person p1 = new Person("Alice", 30);//初始化一个对象
     Person p2 = new Person("Bob", 25);
-    System.out.println(p1.getName() + " " + p1.getAge());
+    System.out.println(p1.getName() + " " + p1.getAge());//调用对象的成员方法
+    System.out.println(p2.getName() + " " + p2.getAge());
 }
