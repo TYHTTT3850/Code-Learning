43832975375f1860b5f203652cb5e2570e22c707
TYHTTT3850 <Tyh11811@outlook.com>
2025-12-26 02:05:00 +0800

    Create 03-指针与引用作为函数参数.cpp

---
+++ Cpp-Learning/C++学习代码/04-函数/03-指针与引用作为函数参数.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//指针作为参数
+void ParaPtr(const int* x,const int* y) {
+    cout << "x: " << *x << ", y: " << *y << endl;
+    cout <<x<< ", " << y << endl;
+}
+
+//引用作为参数
+void ParaRef(const int& x,const int& y) {
+    cout << "x: " << x << ", y: " << y << endl;
+    cout <<&x<< ", " << &y << endl;
+}
+
+void ChangeValPtr(int* x,int* y) {
+    *x = 600;
+    *y = 700;
+}
+
+void ChangeValRef(int& x,int& y) {
+    x = 640;
+    y = 740;
+}
+
+int main() {
+    int x{99};
+    int y{88};
+    ParaPtr(&x, &y);//传递地址
+    ParaRef(x, y);//传递变量
+    cout << "------------------" << endl;
+    ChangeValPtr(&x, &y);//指针传递会改变原变量的值
+    cout << "x: " << x << ", y: " << y << endl;
+    ChangeValRef(x, y);//引用传递也会改变原变量的值
+    cout << "x: " << x << ", y: " << y << endl;
+    return 0;
+}
