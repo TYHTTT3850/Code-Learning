5da9dfa2c2245b2d812a8c22aa360b21c2fbd8dc
TYHTTT3850 <TaoYuhang20040626@outlook.com>
2026-01-27 22:31:53 +0800

    重排文件

--- Python-Learning/Python案例练习/秦九韶算法.py
+++
@@ -1,9 +0,0 @@
-# 秦九韶算法：一种简化多项式运算的算法。
-def qin_jiu_shao(list,x):
-    result=list[0]
-    for a in list[1:]:
-        result=result*x+a
-    return result
-x = int(input())
-list=[4,2,3.5,-2.6,1.7,-0.8] #可用循环语句依次输入多项式系数 
-print(qin_jiu_shao(list,x))
