bc455b75022af93ac408cd8906a8f185611185c7
TYHTTT3850 <TaoYuhang20040626@outlook.com>
2026-02-22 01:44:31 +0800

    Create 03-变量和方法的内存地址分配示例代码.java

---
+++ Java-Learning/Ch02 Java原理/03-变量和方法的内存地址分配示例代码.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    public static void main(String[] args) {//main进栈
+        int a = 10;
+        int b = 20;
+        swap(a, b);//swap进栈，执行完出栈，而里面的a和b是swap方法的局部变量，和main方法的a和b没有关系
+        System.out.println("a: " + a + ", b: " + b); // a: 10, b: 20
+    }
+}
