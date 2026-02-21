a60847b6b61cd6218f763f4c169318a1385e0b92
TYHTTT3850 <TaoYuhang20040626@outlook.com>
2026-02-22 00:39:31 +0800

    Update 01-最基本的功能单元—方法.java

--- Java-Learning/Ch01 基本语法/01-最基本的功能单元—方法.java
+++ Java-Learning/Ch01 基本语法/01-最基本的功能单元—方法.java
@@ -1,15 +1,21 @@
-public class Function {
-	   //方法是java最基本的功能单元
+public class Method {
+    //方法是java最基本的功能单元
+    /*
+    基本的方法的定义格式：
+    public static 返回值类型 方法名称(参数列表){
+        方法体
+    }
+    */
     public static int sum(int a,int b){
 		      return a+b;
 	   }
     //方法可以重载
     //重载是指同名函数具有不同的参数列表，注意：返回值类型不同不是重载，而是错误
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public int add(int a, int b, int c) {
+    public static int add(int a, int b, int c) {
         return a + b + c;
     }
 
@@ -17,13 +23,17 @@ public class Function {
     public static int division(int a,int b){
         if(b==0){
             System.out.println("除数不能为0");
-            return;
+            return -1;
         }
         return a/b;
     }
 
     public static void main(String []args) {
         int s = sum(3,5);
-		      System.out.println(s);
+        int d1 = add(1,2);// 重载
+        int d2 = add(1,2,3);
+        System.out.println(s);
+        System.out.println(d1);
+        System.out.println(d2);
     }
 }
