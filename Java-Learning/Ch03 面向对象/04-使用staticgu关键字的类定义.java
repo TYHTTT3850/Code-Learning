0cbe204888282b0fb3fd4b16bf3fcb2c2c95f1cb
TYHTTT3850 <TaoYuhang20040626@outlook.com>
2026-02-23 03:50:16 +0800

    Create 04-使用staticgu关键字的类定义.java

---
+++ Java-Learning/Ch03 面向对象/04-使用staticgu关键字的类定义.java
@@ -0,0 +1,12 @@
+/*
+static 关键字要点：
+    1.静态变量：被当前所有对象共享。共享：当一个对象修改了静态变量的值，其他对象也会受到影响。
+    2.调用方法：
+        (1).类名调用(推荐)
+        (2).对象名调用(不推荐)
+*/
+public class Student{
+    String name;
+    int age;
+    static String teacher;//所有学生共享一个老师
+}
