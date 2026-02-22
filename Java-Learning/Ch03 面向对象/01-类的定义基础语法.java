b70af13d954d1a9d4536d4bc7f9921dc438c977f
TaoYuhang <TaoYuhang20040626@outlook.com>
2026-02-22 13:38:54 +0800

    Update 01-类的定义基础语法.java

--- Java-Learning/Ch03 面向对象/01-类的定义基础语法.java
+++ Java-Learning/Ch03 面向对象/01-类的定义基础语法.java
@@ -1,7 +1,14 @@
 public class Person{//创建一个类，单独定义在Person.java文件中，不和 main 函数写一起
     String name;
     int age;
-
+    /*
+    构造方法注意事项：
+    1.如果没有定义构造方法，系统给出默认的无参构造方法
+    2.如果自己写了构造方法，系统不提供默认的构造方法
+    3.养成习惯，都手动写无参构造方法和全参数构造方法
+    */
+    public Person(){}
+    
     public Person(String name, int age){//类的构造方法
         this.name = name;//this代表当前对象
         this.age = age;//成员变量
