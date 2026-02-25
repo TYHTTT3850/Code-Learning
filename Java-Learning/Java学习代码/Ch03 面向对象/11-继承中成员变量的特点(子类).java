2f29ad3c3c4ad818842a8e793512289b00837895
TaoYuhang <TaoYuhang20040626@outlook.com>
2026-02-26 00:56:16 +0800

    Update 11-继承中成员变量的特点(子类).java

--- Java-Learning/Java学习代码/Ch03 面向对象/11-继承中成员变量的特点(子类).java
+++ Java-Learning/Java学习代码/Ch03 面向对象/11-继承中成员变量的特点(子类).java
@@ -6,5 +6,9 @@ public class Child extends Parent {
         System.out.println(this.x);        // 20 → 访问子类自己的成员变量
         System.out.println(super.x);  // 10 → 通过 super 访问父类成员变量
         // super 只能在子类内部使用，用于显式指定父类成员
+        System.out.println(y);//由于子类没有定义y，所以直接访问父类的y
+        //与下面两种写法等价
+        //System.out.println(this.y);
+        //System.out.println(super.y);
     }
 }
